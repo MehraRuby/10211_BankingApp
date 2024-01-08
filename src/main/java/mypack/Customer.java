d39bd6f8ddaa89943e44d6d81b95c5d727e2fddb
Ruby <rubymehra.jr@gmail.com>
2024-01-08 21:03:00 +0530

    Second Commit

--- src/main/java/mypack/Customer.java
+++ src/main/java/mypack/Customer.java
@@ -10,7 +10,17 @@ class Customer
 	String contact;
 	String email;
 	
+	public Customer() {}
+	public Customer(int custid, int accno, long initial_amt, long total_amt, String name, String contact,
+			String email) {
+		super();
+		this.custid = custid;
+		this.accno = accno;
+		this.initial_amt = initial_amt;
+		this.total_amt = total_amt;
+		this.name = name;
+		this.contact = contact;
+		this.email = email;
+	}
 	
-	
-
 }
